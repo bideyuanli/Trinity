0cb06b31c6a59e3cdb826287c7d0e2a3f899c2f9
Pipixia <bideyuanli@gmail.com>
2017-12-13 12:18:12 -0500

    瞬爆

--- Routines/Crusader/CrusaderEndlessCondemn.cs
+++ Routines/Crusader/CrusaderEndlessCondemn.cs
@@ -27,12 +27,11 @@ namespace Trinity.Routines.Crusader
         {
             Sets = new Dictionary<Set, SetBonus>
             {
-                { Sets.SeekerOfTheLight, SetBonus.Third }
+                { Sets.ArmorOfAkkhan, SetBonus.Third }
             },
             Items = new List<Item>
             {
-                Legendary.JohannasArgument,
-                Legendary.GuardOfJohanna,
+                Legendary.FrydehrsWrath,
             },
         };
 
@@ -40,21 +39,11 @@ namespace Trinity.Routines.Crusader
 
         public TrinityPower GetOffensivePower()
         {
-            TrinityPower power;
-            TrinityActor target;
-
             if (IsSteedCharging)
                 return null;
-            if (TrySpecialPower(out power))
-                return power;
 
-            if (!Skills.Crusader.Punish.IsBuffActive && ShouldPunish(out target))
-                return Punish(target);
-
-            if (ShouldSlash(out target))
-                return Slash(target);
-            if (IsNoPrimary)
-                return Walk(CurrentTarget);
+            if (ShouldCondemn())
+                return Condemn();
 
             return null;
         }
@@ -66,19 +55,14 @@ namespace Trinity.Routines.Crusader
 
         public TrinityPower GetBuffPower()
         {
-            TrinityPower power;
-
+            if (Player.IsInTown)
+                return null;
             if (IsSteedCharging)
                 return null;
+            TrinityPower power;
 
             if (ShouldAkaratsChampion())
                 return AkaratsChampion();
-
-            if (ShouldProvoke())
-                return Provoke();
-
-            //if (ShouldIronSkin())
-            //    return IronSkin();
 
             if (TryLaw(out power))
                 return power;
@@ -94,106 +78,38 @@ namespace Trinity.Routines.Crusader
         public TrinityPower GetMovementPower(Vector3 destination)
         {
             return null;
-            Vector3 position;
-
-            //Delay settings in ShouldFallingSword will help keep this from being spammed on a Cooldown Pylon
-            //Default range of 20 yards increased to 50 yards. It will search for any 5 mob cluster within 50 yards
-            if (TargetUtil.AnyMobsInRangeOfPosition(destination, 50f, 5) && ShouldFallingSword(out position))
-                return FallingSword(destination);
-
-            if (ShouldSteedCharge())
-                return SteedCharge();
-
-            return Walk(destination);
-        }
-
-        protected override bool ShouldFallingSword(out Vector3 position)
-        {
-            position = Vector3.Zero;
-
-            if (!Skills.Crusader.FallingSword.CanCast())
-                return false;
-
-            //If your health falls below the Emergency Health Percentage in Trinity > Routine settings, cast falling sword again regardless of delay setting.
-            if (Player.CurrentHealthPct < Settings.EmergencyHealthPct)
-                return true;
-
-            //Uses the delay [in milliseconds] defined in Trinity > Routines to keep falling sword from being recast too quickly - Added check for mobs being in Range
-            if (Skills.Crusader.FallingSword.TimeSinceUse < Settings.FallingSwordDelay && TargetUtil.AnyMobsInRange(Settings.FallingSwordMobsRange))
-                return false;
-
-            var target = TargetUtil.GetBestClusterUnit() ?? CurrentTarget;
-            if (target != null)
-            {
-                position = target.Position;
-                return true;
-            }
-
-            return false;
-        }
-
-        protected override bool ShouldBlessedHammer(out TrinityActor target)
-        {
-            target = null;
-
-            if (!Skills.Crusader.BlessedHammer.CanCast())
-                return false;
-
-            //Do not cast Blessed Hammer if Wrath is less than Primary Energy Reserve [default = 25]
-            //This will conserve enough energy for Falling Sword to be cast again
-            if (Player.PrimaryResource <= PrimaryEnergyReserve)
-                return false;
-
-            if (!TargetUtil.AnyMobsInRange(10f))
-                return false;
-
-            target = TargetUtil.GetBestClusterUnit() ?? CurrentTarget;
-            return target != null;
         }
 
 
         protected override bool ShouldProvoke()
         {
-            if (!Skills.Crusader.Provoke.CanCast())
-                return false;
-
-            if (Player.HasBuff(SNOPower.X1_Crusader_Provoke))
-                return false;
-
-            if (!TargetUtil.AnyMobsInRange(15f))
-                return false;
-
-            return true;
+            return false;
         }
         protected override bool ShouldCondemn()
         {
-
             if (!Skills.Crusader.Condemn.CanCast())
                 return false;
 
             if (!TargetUtil.AnyMobsInRange(15f))
                 return false;
+            if (Player.PrimaryResource < 8)
+                return false;
 
+            if (Skills.Crusader.Punish.IsBuffActive)
+                return false;
 
             return Skills.Crusader.Condemn.TimeSinceUse > 525 || Skills.Crusader.Condemn.TimeSinceUse < 475;
         }
+
         protected override bool ShouldIronSkin()
         {
             return false;
-            if (!Skills.Crusader.IronSkin.CanCast())
-                return false;
-
-            if (Player.HasBuff(SNOPower.X1_Crusader_IronSkin))
-                return false;
-
-            if (!TargetUtil.AnyMobsInRange(20f))
-                return false;
-
-            return true;
         }
 
         protected override bool ShouldLawsOfValor()
         {
+            if (!Settings.SpamLawsOfValor)
+                return false;
             if (!Skills.Crusader.LawsOfValor.CanCast())
                 return false;
             double time_to_holy = TimeToElementStart(Element.Holy);
@@ -204,24 +120,12 @@ namespace Trinity.Routines.Crusader
 
         protected override bool ShouldLawsOfJustice()
         {
-            if (!Skills.Crusader.LawsOfJustice.CanCast())
-                return false;
-
-            if (!TargetUtil.AnyMobsInRange(25f))
-                return false;
-
-            return true;
+            return false;
         }
 
         protected override bool ShouldLawsOfHope()
         {
-            if (!Skills.Crusader.LawsOfHope.CanCast())
-                return false;
-
-            if (!TargetUtil.AnyMobsInRange(25f))
-                return false;
-
-            return true;
+            return false;
         }
 
         protected override bool ShouldAkaratsChampion()
@@ -245,19 +149,55 @@ namespace Trinity.Routines.Crusader
 
         public sealed class CrusaderEndlessCondemnSettings : NotifyBase, IDynamicSetting
         {
-            private SkillSettings _akarats;
-            private int _fallingSwordDelay;
-            private int _fallingSwordMobsRange;
-            private int _clusterSize;
-            private float _emergencyHealthPct;
+            private bool _moveToGroundBuffs;
+            [DefaultValue(false)]
+            public bool MoveToGroundBuffs
+            {
+                get { return _moveToGroundBuffs; }
+                set { SetField(ref _moveToGroundBuffs, value); }
+            }
 
-            [DefaultValue(10)]
+            private bool _spamCondemn;
+            [DefaultValue(true)]
+            public bool SpamCondemn
+            {
+                get { return _spamCondemn; }
+                set { SetField(ref _spamCondemn, value); }
+            }
+
+            private bool _spamLawsOfValor;
+            [DefaultValue(true)]
+            public bool SpamLawsOfValor
+            {
+                get { return _spamLawsOfValor; }
+                set { SetField(ref _spamLawsOfValor, value); }
+            }
+
+            private bool _spamProvoke;
+            [DefaultValue(true)]
+            public bool SpamProvoke
+            {
+                get { return _spamProvoke; }
+                set { SetField(ref _spamProvoke, value); }
+            }
+
+            private bool _spamIronSkin;
+            [DefaultValue(true)]
+            public bool SpamIronSkin
+            {
+                get { return _spamIronSkin; }
+                set { SetField(ref _spamIronSkin, value); }
+            }
+
+            private int _clusterSize;
+            [DefaultValue(1)]
             public int ClusterSize
             {
                 get { return _clusterSize; }
                 set { SetField(ref _clusterSize, value); }
             }
 
+            private float _emergencyHealthPct;
             [DefaultValue(0.4f)]
             public float EmergencyHealthPct
             {
@@ -265,33 +205,32 @@ namespace Trinity.Routines.Crusader
                 set { SetField(ref _emergencyHealthPct, value); }
             }
 
+            private SkillSettings _akarats;
             public SkillSettings Akarats
             {
                 get { return _akarats; }
                 set { SetField(ref _akarats, value); }
             }
 
-            [DefaultValue(8000)]
-            public int FallingSwordDelay
+            private SkillSettings _steedCharge;
+            public SkillSettings SteedCharge
             {
-                get { return _fallingSwordDelay; }
-                set { SetField(ref _fallingSwordDelay, value); }
+                get { return _steedCharge; }
+                set { SetField(ref _steedCharge, value); }
             }
-
-            [DefaultValue(15)]
-            public int FallingSwordMobsRange
-            {
-                get { return _fallingSwordMobsRange; }
-                set { SetField(ref _fallingSwordMobsRange, value); }
-            }
-
 
             #region Skill Defaults
 
             private static readonly SkillSettings AkaratsDefaults = new SkillSettings
             {
-                UseMode = UseTime.Selective,
+                UseMode = UseTime.Always,
                 Reasons = UseReasons.Elites | UseReasons.HealthEmergency,
+            };
+
+            private static readonly SkillSettings SteedChargeDefaults = new SkillSettings
+            {
+                UseMode = UseTime.Default,
+                Reasons = UseReasons.Blocked
             };
 
             #endregion
@@ -300,6 +239,7 @@ namespace Trinity.Routines.Crusader
             {
                 base.LoadDefaults();
                 Akarats = AkaratsDefaults.Clone();
+                SteedCharge = SteedChargeDefaults.Clone();
             }
 
             #region IDynamicSetting
@@ -318,5 +258,3 @@ namespace Trinity.Routines.Crusader
         #endregion
     }
 }
-
-
